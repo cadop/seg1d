6c47f1c47af2970c8a18a1d19efa3feef91c831a
cadop <cadop@users.noreply.github.com>
2021-07-15 16:58:10 -0400

    Update ex_segment_class.py

--- seg1d/examples/ex_segment_class.py
+++ seg1d/examples/ex_segment_class.py
@@ -9,9 +9,9 @@
     >>> step = 1 #step to use for correlating reference to target data
     >>> #call the segmentation algorithm
     >>> np.around(seg1d.segment_data(r,t,w,minW,maxW,step), 5)
-    array([[207.      240.        0.91242]
-           [342.      381.        0.88019]
-           [ 72.      112.        0.87768]])
+    array([[207.     , 240.     ,   0.91242],
+           [342.     , 381.     ,   0.88019],
+           [ 72.     , 112.     ,   0.87768]])
  
 '''
 
