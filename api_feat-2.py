774b2969c7a121eb96cd07a1f668265a6a0e95b3
cadop <cadop@users.noreply.github.com>
2021-08-13 19:30:43 -0400

    remove html files

--- api_feat-2.py
+++
@@ -1,5 +0,0 @@
-# plot target data
-plt_t = np.asarray( [ x for x in t.values() ] )
-plt.figure(figsize=(15,4)) # doctest: +SKIP
-plt.plot(plt_t.T,alpha=0.5) # doctest: +SKIP
-plt.show() # doctest: +SKIP
